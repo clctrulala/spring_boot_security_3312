c0da69092f266b4e23d37ad75d9fed2bb3dd7020
clctrulala <clctrulala@gmail.com>
2022-05-23 02:27:21 +0300

    implement custom form login

--- src/main/java/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -2,19 +2,35 @@ package spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SuccessUserHandler successUserHandler;
+    private final FailureUserHandler failureUserHandler;
 
-    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, FailureUserHandler failureUserService) {
         this.successUserHandler = successUserHandler;
+        this.failureUserHandler = failureUserService;
     }
 
     @Override
@@ -22,16 +38,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 //.antMatchers("/", "/index").permitAll()
-                .antMatchers("/**", "/index").denyAll()
+                //.antMatchers("/").permitAll()
                 .antMatchers("/user").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers("/admin/**", "/user/**").hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().successHandler(successUserHandler)
-                .loginPage("/login")
-                .permitAll()
+                .failureHandler(failureUserHandler)
                 .and()
                 .logout()
+                //.logoutUrl("/")
+                .deleteCookies("JSESSIONID")
                 .logoutSuccessUrl("/")
                 .permitAll();
     }
@@ -39,5 +56,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authProvider(PasswordEncoder encoder, UserDetailsService userDetails) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encoder);
+        provider.setUserDetailsService(userDetails);
+        return provider;
     }
 }
--- src/main/java/spring/boot_security/demo/controller/LoginController.java
+++ src/main/java/spring/boot_security/demo/controller/LoginController.java
@@ -3,13 +3,15 @@ package spring.boot_security.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
 
     @GetMapping
-    public String login() {
+    public String login(@RequestParam(required = false) String error) {
+        if(null != error) { return "error"; }
         return "login";
     }
 }
